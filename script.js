286edb69d52f3e5c52c0b79399f9b645f50d6ff4
Paul Vilchez <vilchez.paul@gmail.com>
2015-02-10 14:07:40 -0500

    init GC prototype

---
+++ script.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    var burger = new svgIcon( document.querySelector( '.si-icon-hamburger-cross' ), svgIconConfig, {
+    speed : 200, // animation speed
+    easing : mina.linear, // animation esing
+    size : { w : 30, h : 55 }, // size
+    } );
+    $(document).foundation({
+      offcanvas : {
+        open_method: 'overlap_single'
+      }
+    });
+    $(document).foundation();
+    $(document)
+      .on('open.fndtn.offcanvas', '[data-offcanvas]', function() {
+        $('.left-off-canvas-menu').css('position','fixed');
+        $('.left-off-canvas-menu').css('top', $('.fixed_wrapper').height());
+        $('.left-off-canvas-menu').css('height',$(window).height()-$('.fixed_wrapper').height());
+        $(document.body).trigger("sticky_kit:recalc");
+      })
+      .on('close.fndtn.offcanvas', '[data-offcanvas]', function() {
+          window.setTimeout(function(){$('.left-off-canvas-menu').attr('style','')}, 500);
+          if (burger.toggled=true) { burger.toggle(true)}
+      })
+});
